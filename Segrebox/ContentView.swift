57bc42b57b7eb17546917d8377d5a92b2d38839e
randjg <randyjgunawan1@gmail.com>
2023-10-21 18:03:32 +0700

    Initial Commit

---
+++ Segrebox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Segrebox
+//
+//  Created by Randy Julian on 21/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Segrebox/SegreboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegreboxApp.swift
+//  Segrebox
+//
+//  Created by Randy Julian on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SegreboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
