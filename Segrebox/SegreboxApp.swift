6a451dcec68103ca47bb4f7ab8f3a7f87cd2a972
Eric Prasetya Sentosa <eric.sentosa@binus.ac.id>
2023-10-30 15:04:21 +0700

    init firebae config

--- Segrebox/SegreboxApp.swift
+++ Segrebox/SegreboxApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SegreboxApp: App {
+    init() {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+    }
     var body: some Scene {
         WindowGroup {
             TestViewEvent()
