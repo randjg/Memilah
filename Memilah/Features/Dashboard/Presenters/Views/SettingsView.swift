fa7d0c045b2edc71c9616939f36c2468a45513b9
Eric Prasetya Sentosa <eric.sentosa@binus.ac.id>
2023-11-23 00:13:31 +0700

    deleting unused file and folder
    dashboad viewmodel
    language manager

--- Memilah/Features/Dashboard/Presenters/Views/SettingsView.swift
+++ Memilah/Features/Dashboard/Presenters/Views/SettingsView.swift
@@ -10,78 +10,71 @@ import SwiftUI
 struct SettingsView: View {
     @AppStorage("isDarkMode") private var isDarkMode = false
     @AppStorage("language") private var language = "en_US"
-//    @Binding var isDarkMode: Bool
     @State var isEnglish = true
     @State var isBahasaIndonesia = false
-//    @EnvironmentObject var languageManager: LanguageManager
     var body: some View {
-//        NavigationStack {
-            VStack() {
-                HStack{
-                    Text("Settings")
+        VStack() {
+            HStack{
+                Text("Settings")
+                    .font(
+                        Font.custom(Fonts.plusJakartaSansBold, size: 31)
+                    )
+                Spacer()
+            }
+            .padding(.bottom, 60)
+            
+            HStack{
+                Text("Display")
+                    .font(
+                        Font.custom(Fonts.plusJakartaSansBold, size: 21)
+                    )
+                Spacer()
+            }
+            .padding(.bottom, 31)
+            
+            HStack{
+                Toggle(isOn: $isDarkMode, label: {
+                    Text("Dark Mode")
                         .font(
-                            Font.custom(Fonts.plusJakartaSansBold, size: 31)
+                            Font.custom(Fonts.plusJakartaSansRegular, size: 16)
+                                .weight(.medium)
                         )
-                    Spacer()
-                }
-                .padding(.bottom, 60)
+                })
+                .tint(.blueNormal)
+            }
+            .padding(.bottom, 60)
+            
+            HStack{
+                Text("Choose a language and region")
+                    .font(
+                        Font.custom(Fonts.plusJakartaSansBold, size: 21)
+                    )
+                Spacer()
+            }
+            .padding(.bottom, 25)
+            
+            HStack(spacing: 53) {
+                LanguageButtonComponent(action: {
+                    isEnglish = true
+                    isBahasaIndonesia = false
+                    language = "en_US"
+                }, language: "English", region: "United States", isOn: $isEnglish)
                 
-                HStack{
-                    Text("Display")
-                        .font(
-                            Font.custom(Fonts.plusJakartaSansBold, size: 21)
-                        )
-                    Spacer()
-                }
-                .padding(.bottom, 31)
                 
-                HStack{
-                    Toggle(isOn: $isDarkMode, label: {
-                        Text("Dark Mode")
-                            .font(
-                                Font.custom(Fonts.plusJakartaSansRegular, size: 16)
-                                    .weight(.medium)
-                            )
-                    })
-                    .tint(.blueNormal)
-                }
-                .padding(.bottom, 60)
-                
-                HStack{
-                    Text("Choose a language and region")
-                        .font(
-                            Font.custom(Fonts.plusJakartaSansBold, size: 21)
-                        )
-                    Spacer()
-                }
-                .padding(.bottom, 25)
-                
-                HStack(spacing: 53) {
-                    LanguageButtonComponent(action: {
-                        isEnglish = true
-                        isBahasaIndonesia = false
-//                        languageManager.selectedLanguage = Locale(identifier: "en_US")
-                        language = "en_US"
-                    }, language: "English", region: "United States", isOn: $isEnglish)
-                    
-                    
-                    LanguageButtonComponent(action: {
-                        isEnglish = false
-                        isBahasaIndonesia = true
-//                        languageManager.selectedLanguage = Locale(identifier: "id_ID")
-                        language = "id_ID"
-                    }, language: "Bahasa Indonesia", region: "Indonesia", isOn: $isBahasaIndonesia)
-                    
-                    Spacer()
-                }
+                LanguageButtonComponent(action: {
+                    isEnglish = false
+                    isBahasaIndonesia = true
+                    language = "id_ID"
+                }, language: "Bahasa Indonesia", region: "Indonesia", isOn: $isBahasaIndonesia)
                 
                 Spacer()
             }
-            .padding(.horizontal, 79)
-            .padding(.top, 77)
-            .ignoresSafeArea()
-//        }
+            
+            Spacer()
         }
+        .padding(.horizontal, 79)
+        .padding(.top, 77)
+        .ignoresSafeArea()
         .onAppear {
             if language == "en_US" {
                 isEnglish = true
@@ -94,8 +87,7 @@ struct SettingsView: View {
     }
 }
 
+
 #Preview {
     SettingsView()
-//        .environmentObject(LanguageManager())
-//    SettingsView(isDarkMode: .constant(false))
 }
